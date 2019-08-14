072d8ab78a1cee1051f62e9581103574b6ae0c2b
aburd <burdick.aaron@gmail.com>
2019-08-14 16:52:22 +0900

    init commit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path')
+const webpack = require('webpack')
+
+const dev = process.env.NODE_ENV === 'development'
+const plugins = []
+plugins.push(
+  new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('development') } })
+)
+
+const resolvedPaths = ['../src/lib', '../packs', '../src', '../../assets'].map(p => path.resolve(__dirname, p))
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/main.ts'),
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'target'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css|scss?$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: { loader: 'babel-loader' },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.jsx', '.js'],
+    modules: [...resolvedPaths, 'node_modules'],
+  },
+  plugins,
+  devtool: dev ? 'inline-source-map' : 'none',
+}; 
